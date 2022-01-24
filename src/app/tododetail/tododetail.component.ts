1547cbedb4034a80638be7432d06a1bc840e658e
noxob <samirusta18@gmail.com>
2022-01-25 00:30:16 +0300

    added animations

--- src/app/tododetail/tododetail.component.ts
+++ src/app/tododetail/tododetail.component.ts
@@ -1,8 +1,37 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from '../entity/todo';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-tododetail',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '40px',
+        opacity: 1,
+        //display: "block"
+      })),
+      state('closed', style({
+        height: '25px',
+        opacity: 0,
+        //display: "none"
+      })),
+      transition('open => closed', [
+        animate('0.5s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
   templateUrl: './tododetail.component.html',
   styleUrls: ['./tododetail.component.scss']
 })
@@ -10,10 +39,15 @@ export class TododetailComponent implements OnInit {
 
   @Input()
   todo: Todo;
+  showButtons:boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(){
+    this.showButtons=!this.showButtons;
   }
 
 }
