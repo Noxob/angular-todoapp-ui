45a9e2d5599fdde8c6ddf372e8ed6365b8a13200
noxob <samirusta18@gmail.com>
2022-01-31 00:16:48 +0300

    project name changed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/couchbase-demo-todoapp-ui'),
+      dir: require('path').join(__dirname, './coverage/angular-todoapp-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
