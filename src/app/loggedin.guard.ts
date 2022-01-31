1ffdc3871a8f8d9a4375fef2df08643cd36ef2cf
noxob <samirusta18@gmail.com>
2022-02-01 00:06:39 +0300

    snackbar removed

--- src/app/loggedin.guard.ts
+++ src/app/loggedin.guard.ts
@@ -1,18 +1,14 @@
 import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
   notLoggedIn:boolean=false;
-  constructor(private authService: AuthService,private router: Router, private snackBar: MatSnackBar) { }
+  constructor(private authService: AuthService,private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     this.authService.isLoggedIn.subscribe(isLoggedIn =>  {
         if (isLoggedIn) {
-            this.snackBar.open("You are already logged in.", "OK", {
-              duration: 2000,
-            });
           this.router.navigate(["/"]);
           this.notLoggedIn=false;
           return false;
